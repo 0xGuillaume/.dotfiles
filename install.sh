be978471a8eb77a0b056dd568791cac4262c23db
Guillaume <65726796+0xGuillaume@users.noreply.github.com>
2022-02-24 09:16:07 +0100

    [Install] Added user to dockergrp

--- install.sh
+++ install.sh
@@ -59,6 +59,8 @@ sudo dnf install dnf-plugins-core -y
 sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 sudo dnf install docker-ce docker-ce-cli containerd.io -y
 sudo systemctl enable docker
+sudo usermod -aG docker $USER
+newgrp docker
 sudo snap install remmina -y
 
 
