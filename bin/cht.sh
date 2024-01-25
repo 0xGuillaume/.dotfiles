767768fef33d285cf34954e37f02e488e3c5aad2
0xGuillaume <65726796+0xGuillaume@users.noreply.github.com>
2024-01-25 13:42:33 +0100

    Added cheatsheet script

---
+++ bin/cht.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/fish
+
+set selected $(cat ../tmux/.tmux-cht-command ../tmux/.tmux-cht-languages | fzf)
+
+if test -z $selected
+    exit 0
+end
+
+read -P "Enter query: " query
+
+if grep -qs "$selected" ../tmux/.tmux-cht-languages
+    set query $(echo $query | tr " " "+")
+
+    set url "cht.sh/$selected/$query"
+    tmux split-window -h fish -c "curl -s $url | less -R"
+
+else
+    set url "cht.sh/$selected~$query"
+    tmux split-window -h fish -c "curl -s $url | less -R"
+end
+
